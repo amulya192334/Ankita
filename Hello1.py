706095a9d416dd758e05b15a5254664e1c817dda
ankii19 <ankita24rose@gmail.com>
2025-02-11 09:59:49 +0530

    Update Hello1.py

--- Hello1.py
+++ Hello1.py
@@ -2,3 +2,5 @@ num1=float(input("Enter the first number:"))
 num2=float(input("Enter the second number:"))
 result=num1-num2
 print("The subtraction of two numbers:",result)
+print("The subtraction of two numbers:",result)
+
